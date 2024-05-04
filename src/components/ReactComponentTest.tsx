710c2875cd7472b5e6cfa01c98ec94d3e7e5615a
yudai hiyama <yudai1204h@gmail.com>
2024-05-05 00:04:23 +0900

    first commit

---
+++ src/components/ReactComponentTest.tsx
@@ -0,0 +1,13 @@
+import { css } from "../../styled-system/css";
+
+export const ReactComponentTest = () => {
+  return (
+    <div
+      className={css({
+        background: "blue",
+      })}
+    >
+      ReactComponentTest
+    </div>
+  );
+};
