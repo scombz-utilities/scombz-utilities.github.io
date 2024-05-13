b39b6fe90176e805e2324d7cf20705c1bd24687b
yudai hiyama <yudai1204h@gmail.com>
2024-05-13 18:37:09 +0900

    change domain to ".com"

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://scombz-utilities.github.io",
+  site: "https://scombz-utilities.com",
+  // site: "https://scombz-utilities.github.io",
   // base: "/",
 });
